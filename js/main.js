9ff2fc654194bb5b9257bda51e7d50fc08f81538
ZvOnKiYSMASH <abdiramanovlenur@gmail.com>
2022-02-11 18:03:12 +0500

    coding

--- js/main.js
+++ js/main.js
@@ -205,7 +205,7 @@
 // }
 
 
-// Task 5 
+// Task 5
 // Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word.Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 // Example
 // ['hello', 'world', 'this', 'is', 'great']  => 'hello world this is great'
@@ -339,6 +339,94 @@
 // }
 
 // console.log(differenceInAges([57, 99, 14, 32]));
+
+
+// Task 10
+// All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+// У всех животных праздник! Каждое животное приносит одно блюдо. Есть только одно правило: блюдо должно начинаться и заканчиваться теми же буквами, что и имя животного. Например, большая голубая цапля приносит чесночный наан, а синица приносит шоколадный торт.
+// Напишите функцию feast, которая принимает имя животного и блюдо в качестве аргументов и возвращает true или false, чтобы указать, разрешено ли животному принести блюдо на пир.
+// Предположим, что beastи dishвсегда строчные строки, и каждая из них состоит как минимум из двух букв. beastи dishможет содержать дефисы и пробелы, но они не будут отображаться в начале или конце строки. Они не будут содержать цифр.
+
+// 1-option
+// function feast(beast, dish) {
+//     const dishStrs = dish.split('');
+//     const beastStrs = beast.split('');
+//     let flag = false;
+
+//     if ((beastStrs[0] === dishStrs[0] && (beastStrs[beastStrs.length - 1] === dishStrs[dishStrs.length - 1]))) {
+//         flag = true;
+//     } else {
+//         flag = false;
+//     }
+
+//     return flag;
+// };
+
+
+// 2-option
+// function feast(beast, dish) {
+//     const dishStrs = dish.split('');
+//     const beastStrs = beast.split('');
+
+//     return (beastStrs[0] === dishStrs[0] && (beastStrs[beastStrs.length - 1] === dishStrs[dishStrs.length - 1]));
+// };
+
+// 3-option
+// function feast(beast, dish) {
+//     const dishStrs = dish.split('');
+//     const beastStrs = beast.split('');
+
+//     return (beastStrs.shift() === dishStrs.shift()) && (beastStrs.pop() === dishStrs.pop());
+// };
+
+// console.log(feast("brown bear", "bear claw"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 7 KYU -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
